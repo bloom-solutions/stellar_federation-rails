a6bca494036df3927f70ef58e6fc3d1b399a5d3b
Ace Subido <ace.subido@gmail.com>
2018-04-24 19:38:32 +0800

    Pass prepared query object instead of query_params to callback class

--- spec/services/stellar_federation/queries/trigger_on_query_callback_class_spec.rb
+++ spec/services/stellar_federation/queries/trigger_on_query_callback_class_spec.rb
@@ -3,24 +3,24 @@ require "spec_helper"
 module StellarFederation
   module Queries
     RSpec.describe TriggerOnQueryCallbackClass do
+      let(:query) { Query.new(address_id: "foo") }
 
       context "with configured callback class" do
         let(:query_response) { QueryResponse.new }
+
         it "triggers the configured callback class" do
           expect(ProcessStellarFederationQuery).to receive(:call)
-            .with(address_name: 'foo').and_return(query_response)
-          described_class.execute(query_params: { address_name: "foo" })
+            .with(query).and_return(query_response)
+          described_class.execute(query: query)
         end
       end
 
       context "with configured callback class and doesnt return QueryResponse" do
         it "triggers the configured callback class" do
           expect(ProcessStellarFederationQuery).to receive(:call)
-            .with(address_name: 'foo').and_return("test")
+            .with(query).and_return("test")
 
-          expect {
-            described_class.execute(query_params: { address_name: "foo" })
-          }.to raise_error(
+          expect { described_class.execute(query: query) }.to raise_error(
             StandardError,
             "ProcessStellarFederationQuery must return a StellarFederation::QueryResponse"
           )
@@ -28,14 +28,22 @@ module StellarFederation
       end
 
       context "without configured callback class" do
-        it "fails and returns an error" do
+        let(:query) { Query.new(address_id: "foo") }
+
+        before do
           StellarFederation.configure do |c|
             c.on_query = ""
           end
+        end
 
-          expect {
-            described_class.execute(query_params: { address_name: "foo" })
-          }.to raise_error(
+        after do
+          StellarFederation.configure do |c|
+            c.on_query = "ProcessStellarFederationQuery"
+          end
+        end
+
+        it "fails and returns an error" do
+          expect { described_class.execute(query: query) }.to raise_error(
             NameError,
             "StellarFederation.on_query isn't configured",
           )
