d81f2b047b12e9f8a41d87ea0787f7b0ec76b76a
Ramon Tayag <ramon.tayag@gmail.com>
2018-05-28 22:43:58 +0800

    Fix CORS -- access from a different domain

--- app/controllers/stellar_federation/api/v1/queries_controller.rb
+++ app/controllers/stellar_federation/api/v1/queries_controller.rb
@@ -2,6 +2,8 @@ module StellarFederation
   module Api
     module V1
       class QueriesController < ApplicationController
+        before_filter :set_cors_headers, only: %i[index]
+
         def index
           result = Queries::ProcessQuery.(query_params: query_params)
 
@@ -29,6 +31,10 @@ module StellarFederation
 
         def query_params
           params.permit(:q, :type).to_hash.with_indifferent_access
+        end
+
+        def set_cors_headers
+          response.headers["Access-Control-Allow-Origin"] = "*"
         end
       end
     end
